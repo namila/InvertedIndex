b0adf96e95e1f792d76b6ce238367d171c89d793
Namila Perera <NamilaP@99x.lk>
2019-04-14 16:58:25 +0530

    Initial commit

---
+++ src/main/java/DataMining/InvertedIndex/InvertedIndexer.java
@@ -0,0 +1,24 @@
+package DataMining.InvertedIndex;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+public class InvertedIndexer 
+{
+	private Map<String, LinkedList<Integer>> invertedIndex;
+    
+	public InvertedIndexer() {
+		invertedIndex = new HashMap<String, LinkedList<Integer>>();
+	}
+	
+	public void addFileToInvertedIndex(String filePath){
+		
+	}
+	
+	
+}
---
+++ src/test/java/DataMining/InvertedIndex/InvertedIndexerTest.java
@@ -0,0 +1,38 @@
+package DataMining.InvertedIndex;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class InvertedIndexerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public InvertedIndexerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( InvertedIndexerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
